54344c4092d4e9b0c7eb0db750189c1fdfc57fef
Diego Rangel <diego.analisesg@gmail.com>
2019-12-27 13:15:01 -0300

    Ajustes

--- src/DiegoRangel.DotNet.Framework.CQRS.API/Extensions/CommonMessagesExtensions.cs
+++ src/DiegoRangel.DotNet.Framework.CQRS.API/Extensions/CommonMessagesExtensions.cs
@@ -1,13 +1,16 @@
-﻿using DiegoRangel.DotNet.Framework.CQRS.Infra.CrossCutting.Messages;
+﻿using System;
+using DiegoRangel.DotNet.Framework.CQRS.Infra.CrossCutting.Messages;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DiegoRangel.DotNet.Framework.CQRS.API.Extensions
 {
     public static class CommonMessagesExtensions
     {
-        public static void AddCommonMessages(this IServiceCollection services, CommonMessages commonMessages)
+        public static void AddCommonMessages(this IServiceCollection services,  Action<CommonMessages> action)
         {
-            services.AddSingleton(commonMessages);
+            var commomMessages = new CommonMessages();
+            action(commomMessages);
+            services.AddSingleton(commomMessages);
         }
     }
 }
